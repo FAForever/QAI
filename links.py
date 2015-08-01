1d2e3f25f9acda4afeaa5a41462c480705b86963
kevin <Bra1nwash3d@users.noreply.github.com>
2015-08-01 13:42:48 +0200

    adding a bit of description in front of links

--- links.py
+++ links.py
@@ -1,16 +1,16 @@
 LINKS = {
-    "forum":        "http://forums.faforever.com/forums/index.php",
-    "support":      "http://forums.faforever.com/forums/viewforum.php?f=3",
-    "wiki":         "http://wiki.faforever.com/index.php?title=Main_Page",
-    "news":         "http://www.faforever.com/news/",
+    "forum":        "FAF Forum! http://forums.faforever.com/forums/index.php",
+    "support":      "Tech support! http://forums.faforever.com/forums/viewforum.php?f=3",
+    "wiki":         "FAF Wiki! http://wiki.faforever.com/index.php?title=Main_Page",
+    "news":         "What's new? http://www.faforever.com/news/",
 
-    "replay":       "http://replay.faforever.com/ID",
-    "unitsdb":     "http://content.faforever.com/faf/unitsDB/",
-    "namechange":   "http://app.faforever.com/faf/userName.php",
-    "clans":        "http://www.faforever.com/clans/clans_list",
+    "replay":       "Replay link: http://replay.faforever.com/ID",
+    "unitsdb":      "Unit database: http://content.faforever.com/faf/unitsDB/",
+    "namechange":   "Change username: http://app.faforever.com/faf/userName.php",
+    "clans":        "FAF clans! http://www.faforever.com/clans/clans_list",
 
-    "github":       "https://github.com/FAForever",
+    "github":       "FAF on Github: https://github.com/FAForever",
 
-    "replayparser": "http://fafafaf.bitbucket.org/",
-    "tumblr":       "http://fafreactions.tumblr.com/",
+    "replayparser": "Replay parser: http://fafafaf.bitbucket.org/",
+    "tumblr":       "FAF Reactions on tumblr: http://fafreactions.tumblr.com/",
 }
